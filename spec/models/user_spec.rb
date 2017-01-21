1430ff62cf432fbbe4b74b51820fd5409efe7991
Dan Hilton <dhilton@missiondata.com>
2017-01-21 10:52:39 -0500

    added user factory and test for factory create

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	before :each do 
+		@user = create :user
+	end
+
+	it "should have a valid factory" do 
+		expect(@user).to be_valid
+	end
+	
 end
