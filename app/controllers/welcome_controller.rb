ff2bf578844569a331c62999b8e6ff69ed01f7bf
Dan Hilton <dhilton@missiondata.com>
2017-01-22 11:57:19 -0500

    modify welcome controller to handle landing and dashboard pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,6 @@ class WelcomeController < ApplicationController
 	def index
 		if current_user.present?
 			render :dashboard
-		else
-			render :home
 		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'welcome#index'
-  resources :products
+	devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+	root 'welcome#index'
+	resources :products
+	resources :orders, only: [:new, :create, :show]
 end
