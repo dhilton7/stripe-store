6730b4be2f1292a5feb2307c922d47bb0c1eda39
Dan Hilton <dhilton@missiondata.com>
2017-01-22 12:02:40 -0500

    added orders controller and associated views for new & show

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,45 @@
+class OrdersController < ApplicationController
+
+	def new
+		@order = Order.new
+		@product = Product.find params[:pid]
+	end
+
+	def create
+		token = params[:stripeToken]
+		amount = (order_params[:amount].to_f * 100).to_i
+		product = Product.find order_params[:product_id]
+
+		charge = Stripe::Charge.create({
+			amount: 		amount,
+			description: 	"Charge for #{product}",
+			currency: 		'usd',
+			source: 		token,
+			application_fee: (amount * Order::APP_FEE).to_i,
+			destination: User.last.uid 	
+		})
+
+		new_params = order_params.merge({
+			card_token: token,
+			customer_id: charge.source.id,
+			stripe_charge_id: charge.id,
+			email: params[:stripeEmail] 
+		})
+		order = Order.create new_params
+		redirect_to order_path(order)
+	
+	rescue Stripe::CardError => e
+		flash[:error] = e.message
+		redirect_to products_path
+	end
+
+	def show
+		@order = Order.find params[:id]
+	end
+
+	private
+
+	def order_params
+		params.require(:order).permit(:product_id, :amount)
+	end
+end
---
+++ app/models/order.rb
@@ -0,0 +1,4 @@
+class Order < ActiveRecord::Base
+
+	APP_FEE = 0.1
+end
