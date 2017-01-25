1780ed3ff87a1566630a483a05aa41d99ae57df6
Dan Hilton <dhilton@missiondata.com>
2017-01-25 00:21:18 -0500

    separate dashboard for connected and unconnected users. add pagination to dashboard view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
 
 	def dashboard
 		redirect_to root_path if current_user.nil?
+		@products = current_user.products.order('updated_at DESC').paginate(page: params[:page])
 	end
 end
